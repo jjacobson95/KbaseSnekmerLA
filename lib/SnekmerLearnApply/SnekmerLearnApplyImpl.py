99379bb87011c184ae76b87fad518358745c22e5
Jeremy <jeremy.jacobson3402@gmail.com>
2024-02-07 10:12:49 -0800

    fixing report bug

--- lib/SnekmerLearnApply/SnekmerLearnApplyImpl.py
+++ lib/SnekmerLearnApply/SnekmerLearnApplyImpl.py
@@ -67,9 +67,10 @@ This will have to be changed soon.
         input_seqs = params['input_seqs']
 
         report = KBaseReport(self.callback_url)
-        report_info = report.create({'report': {'objects_created':[],
-                                                'text_message': params['input_seqs']},
-                                                'workspace_name': params['workspace_name']})
+        text_message = '\n'.join(params['input_seqs'])
+        report_info = report.create({'report': {'objects_created': [],
+                                        'text_message': text_message},
+                                        'workspace_name': params['workspace_name']})
         output = {
             'report_name': report_info['name'],
             'report_ref': report_info['ref'],
